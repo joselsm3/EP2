017a0682a2b137513c81885fdaf992fd961bfb09
augustoda1 <augustoda@al.insper.edu.br>
2024-05-01 20:27:50 -0300

    ajustes

--- dev2.py
+++ dev2.py
@@ -6,6 +6,57 @@ numletra={0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J'}
 lista_denumeros=['1','2','3','4','5']
 letras=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 d={'a':0, 'b':1, 'c':2, 'd':3, 'e':4, 'f':5, 'g':6, 'h':7, 'i':8, 'j':9}
+def printa_string_bombardeio(l1,l2,lista):
+    for i in range(len(lista)): 
+        for l in range(len(lista[i])): 
+            if l1[i][l] == "X": 
+                cor = "X"
+                l1[i][l] = DICIONARIO_CORES[cor]
+            elif l1[i][l] == "A": 
+                cor = "A"
+                l1[i][l] = DICIONARIO_CORES[cor]
+    for i in range(len(l2)): 
+        for l in range(len(l2[i])): 
+            if l2[i][l] == "A": 
+                cor = "A"
+                l2[i][l] = DICIONARIO_CORES[cor]
+            elif l2[i][l] == "X": 
+                cor = "X"
+                l2[i][l] = DICIONARIO_CORES[cor]
+    print(f"        COMPUTADOR -                                                        JOGADOR -")
+    print("    A  B  C  D  E  F  G  H  I  J                                 A  B  C  D  E  F  G  H  I  J ")
+    print(f'  1{l1[0][0]}{l1[0][1]}{l1[0][2]}{l1[0][3]}{l1[0][4]}{l1[0][5]}{l1[0][6]}{l1[0][7]}{l1[0][8]}{l1[0][9]}1                             1{l2[0][0]}{l2[0][1]}{l2[0][2]}{l2[0][3]}{l2[0][4]}{l2[0][5]}{l2[0][6]}{l2[0][7]}{l2[0][8]}{l2[0][9]}1')
+    print(f'  2{l1[1][0]}{l1[1][1]}{l1[1][2]}{l1[1][3]}{l1[1][4]}{l1[1][5]}{l1[1][6]}{l1[1][7]}{l1[1][8]}{l1[1][9]}2                             2{l2[1][0]}{l2[1][1]}{l2[1][2]}{l2[1][3]}{l2[1][4]}{l2[1][5]}{l2[1][6]}{l2[1][7]}{l2[1][8]}{l2[1][9]}2')
+    print(f'  3{l1[2][0]}{l1[2][1]}{l1[2][2]}{l1[2][3]}{l1[2][4]}{l1[2][5]}{l1[2][6]}{l1[2][7]}{l1[2][8]}{l1[2][9]}3                             3{l2[2][0]}{l2[2][1]}{l2[2][2]}{l2[2][3]}{l2[2][4]}{l2[2][5]}{l2[2][6]}{l2[2][7]}{l2[2][8]}{l2[2][9]}3')
+    print(f'  4{l1[3][0]}{l1[3][1]}{l1[3][2]}{l1[3][3]}{l1[3][4]}{l1[3][5]}{l1[3][6]}{l1[3][7]}{l1[3][8]}{l1[3][9]}4                             4{l2[3][0]}{l2[3][1]}{l2[3][2]}{l2[3][3]}{l2[3][4]}{l2[3][5]}{l2[3][6]}{l2[3][7]}{l2[3][8]}{l2[3][9]}4')
+    print(f'  5{l1[4][0]}{l1[4][1]}{l1[4][2]}{l1[4][3]}{l1[4][4]}{l1[4][5]}{l1[4][6]}{l1[4][7]}{l1[4][8]}{l1[4][9]}5                             5{l2[4][0]}{l2[4][1]}{l2[4][2]}{l2[4][3]}{l2[4][4]}{l2[4][5]}{l2[4][6]}{l2[4][7]}{l2[4][8]}{l2[4][9]}5')
+    print(f'  6{l1[5][0]}{l1[5][1]}{l1[5][2]}{l1[5][3]}{l1[5][4]}{l1[5][5]}{l1[5][6]}{l1[5][7]}{l1[5][8]}{l1[5][9]}6                             6{l2[5][0]}{l2[5][1]}{l2[5][2]}{l2[5][3]}{l2[5][4]}{l2[5][5]}{l2[5][6]}{l2[5][7]}{l2[5][8]}{l2[5][9]}6')
+    print(f'  7{l1[6][0]}{l1[6][1]}{l1[6][2]}{l1[6][3]}{l1[6][4]}{l1[6][5]}{l1[6][6]}{l1[6][7]}{l1[6][8]}{l1[6][9]}7                             7{l2[6][0]}{l2[6][1]}{l2[6][2]}{l2[6][3]}{l2[6][4]}{l2[6][5]}{l2[6][6]}{l2[6][7]}{l2[6][8]}{l2[6][9]}7')
+    print(f'  8{l1[7][0]}{l1[7][1]}{l1[7][2]}{l1[7][3]}{l1[7][4]}{l1[7][5]}{l1[7][6]}{l1[7][7]}{l1[7][8]}{l1[7][9]}8                             8{l2[7][0]}{l2[7][1]}{l2[7][2]}{l2[7][3]}{l2[7][4]}{l2[7][5]}{l2[7][6]}{l2[7][7]}{l2[7][8]}{l2[7][9]}8')
+    print(f'  9{l1[8][0]}{l1[8][1]}{l1[8][2]}{l1[8][3]}{l1[8][4]}{l1[8][5]}{l1[8][6]}{l1[8][7]}{l1[8][8]}{l1[8][9]}9                             9{l2[8][0]}{l2[8][1]}{l2[8][2]}{l2[8][3]}{l2[8][4]}{l2[8][5]}{l2[8][6]}{l2[8][7]}{l2[8][8]}{l2[8][9]}9')
+    print(f' 10{l1[9][0]}{l1[9][1]}{l1[9][2]}{l1[9][3]}{l1[9][4]}{l1[9][5]}{l1[9][6]}{l1[9][7]}{l1[9][8]}{l1[9][9]}10                           10{l2[9][0]}{l2[9][1]}{l2[9][2]}{l2[9][3]}{l2[9][4]}{l2[9][5]}{l2[9][6]}{l2[9][7]}{l2[9][8]}{l2[9][9]}10')            
+    print("    A  B  C  D  E  F  G  H  I  J                                 A  B  C  D  E  F  G  H  I  J ")
+    return ""
+def printa_string(l1,l2):
+    for i in range(len(l2)): 
+        for l in range(len(l2[i])): 
+            if l2[i][l] == "N": 
+                cor = "V"
+                l2[i][l] = DICIONARIO_CORES[cor]
+    print("        COMPUTADOR                                                       JOGADOR") 
+    print("    A  B  C  D  E  F  G  H  I  J                                 A  B  C  D  E  F  G  H  I  J ")
+    print(f'  1{l1[0][0]}{l1[0][1]}{l1[0][2]}{l1[0][3]}{l1[0][4]}{l1[0][5]}{l1[0][6]}{l1[0][7]}{l1[0][8]}{l1[0][9]}1                             1{l2[0][0]}{l2[0][1]}{l2[0][2]}{l2[0][3]}{l2[0][4]}{l2[0][5]}{l2[0][6]}{l2[0][7]}{l2[0][8]}{l2[0][9]}1')
+    print(f'  2{l1[1][0]}{l1[1][1]}{l1[1][2]}{l1[1][3]}{l1[1][4]}{l1[1][5]}{l1[1][6]}{l1[1][7]}{l1[1][8]}{l1[1][9]}2                             2{l2[1][0]}{l2[1][1]}{l2[1][2]}{l2[1][3]}{l2[1][4]}{l2[1][5]}{l2[1][6]}{l2[1][7]}{l2[1][8]}{l2[1][9]}2')
+    print(f'  3{l1[2][0]}{l1[2][1]}{l1[2][2]}{l1[2][3]}{l1[2][4]}{l1[2][5]}{l1[2][6]}{l1[2][7]}{l1[2][8]}{l1[2][9]}3                             3{l2[2][0]}{l2[2][1]}{l2[2][2]}{l2[2][3]}{l2[2][4]}{l2[2][5]}{l2[2][6]}{l2[2][7]}{l2[2][8]}{l2[2][9]}3')
+    print(f'  4{l1[3][0]}{l1[3][1]}{l1[3][2]}{l1[3][3]}{l1[3][4]}{l1[3][5]}{l1[3][6]}{l1[3][7]}{l1[3][8]}{l1[3][9]}4                             4{l2[3][0]}{l2[3][1]}{l2[3][2]}{l2[3][3]}{l2[3][4]}{l2[3][5]}{l2[3][6]}{l2[3][7]}{l2[3][8]}{l2[3][9]}4')
+    print(f'  5{l1[4][0]}{l1[4][1]}{l1[4][2]}{l1[4][3]}{l1[4][4]}{l1[4][5]}{l1[4][6]}{l1[4][7]}{l1[4][8]}{l1[4][9]}5                             5{l2[4][0]}{l2[4][1]}{l2[4][2]}{l2[4][3]}{l2[4][4]}{l2[4][5]}{l2[4][6]}{l2[4][7]}{l2[4][8]}{l2[4][9]}5')
+    print(f'  6{l1[5][0]}{l1[5][1]}{l1[5][2]}{l1[5][3]}{l1[5][4]}{l1[5][5]}{l1[5][6]}{l1[5][7]}{l1[5][8]}{l1[5][9]}6                             6{l2[5][0]}{l2[5][1]}{l2[5][2]}{l2[5][3]}{l2[5][4]}{l2[5][5]}{l2[5][6]}{l2[5][7]}{l2[5][8]}{l2[5][9]}6')
+    print(f'  7{l1[6][0]}{l1[6][1]}{l1[6][2]}{l1[6][3]}{l1[6][4]}{l1[6][5]}{l1[6][6]}{l1[6][7]}{l1[6][8]}{l1[6][9]}7                             7{l2[6][0]}{l2[6][1]}{l2[6][2]}{l2[6][3]}{l2[6][4]}{l2[6][5]}{l2[6][6]}{l2[6][7]}{l2[6][8]}{l2[6][9]}7')
+    print(f'  8{l1[7][0]}{l1[7][1]}{l1[7][2]}{l1[7][3]}{l1[7][4]}{l1[7][5]}{l1[7][6]}{l1[7][7]}{l1[7][8]}{l1[7][9]}8                             8{l2[7][0]}{l2[7][1]}{l2[7][2]}{l2[7][3]}{l2[7][4]}{l2[7][5]}{l2[7][6]}{l2[7][7]}{l2[7][8]}{l2[7][9]}8')
+    print(f'  9{l1[8][0]}{l1[8][1]}{l1[8][2]}{l1[8][3]}{l1[8][4]}{l1[8][5]}{l1[8][6]}{l1[8][7]}{l1[8][8]}{l1[8][9]}9                             9{l2[8][0]}{l2[8][1]}{l2[8][2]}{l2[8][3]}{l2[8][4]}{l2[8][5]}{l2[8][6]}{l2[8][7]}{l2[8][8]}{l2[8][9]}9')
+    print(f' 10{l1[9][0]}{l1[9][1]}{l1[9][2]}{l1[9][3]}{l1[9][4]}{l1[9][5]}{l1[9][6]}{l1[9][7]}{l1[9][8]}{l1[9][9]}10                           10{l2[9][0]}{l2[9][1]}{l2[9][2]}{l2[9][3]}{l2[9][4]}{l2[9][5]}{l2[9][6]}{l2[9][7]}{l2[9][8]}{l2[9][9]}10')            
+    print("    A  B  C  D  E  F  G  H  I  J                                 A  B  C  D  E  F  G  H  I  J ")
+    return ''
 def cria_mapa(tamanho):
     lista = []
     for i in range(tamanho):
@@ -83,24 +134,24 @@ for k , v in PAISES.items():
 
 mapa_computador = cria_mapa(10)
 mapa_jogador = cria_mapa(10)
-def mostrarMapa(mat1, mat2):
+'''def mostrarMapa(mat1, mat2):
     for i in range(len(mat2)): 
         for l in range(len(mat2[i])): 
             if mat2[i][l] == "N": 
                 cor = "V"
                 mat2[i][l] = DICIONARIO_CORES[cor]
-    print('   A  B  C  D  E  F  G  H  I  J        A  B  C  D  E  F  G  H  I  J')
+    print('   A   B   C   D   E   F   G   H   I   J        A   B   C   D   E   F   G   H   I   J')
     for linha in range(10):
         print(f'{linha+1:2d}', end='')
         for coluna in range(10):
             print(f' {mat1[linha][coluna]} ', end='')
-        print(f'    {linha+1:2d}', end='')
+        print(f'            {linha+1:2d}', end='')
         for coluna in range(10):
             print(f' {mat2[linha][coluna]} ', end='')
         print()
-    print('   A  B  C  D  E  F  G  H  I  J        A  B  C  D  E  F  G  H  I  J')
+    print('   A   B   C   D   E   F   G   H   I   J        A   B   C   D   E   F   G   H   I   J')
 
-mostrarMapa(mapa_computador, mapa_jogador)
+#mostrarMapa(mapa_computador, mapa_jogador)'''
 
 def aloca_navios_jogador(mapa, blocos,linha,coluna,orientacao):      
     if orientacao == 'h':
@@ -164,14 +215,14 @@ for i in navios_usuario:
     navios_usuario_2.append(i)
 print(navios_usuario)
 del navios_usuario_2[0]
-
+lista = cria_mapa(10)
 for i in navios_usuario:
     while True:
         print(f'alocar: {i} {CONFIGURACAO[i]} blocos')
         sleep(1)
         if navios_usuario_2!=[]:
             print(f'próximos: {navios_usuario_2}')
-        letra = input('Informe a letra:')
+        letra = input('Informe a letra:').lower()
         coluna = input('Informe a coluna:')
         direcao = input('Informe a orientação [v|h]').lower()
    
@@ -180,7 +231,7 @@ for i in navios_usuario:
                 if posicao_suporta_usuario(mapa_jogador, CONFIGURACAO[i], int(coluna)-1, coords[letra], direcao):
                     coordenada = converte_coordenada(letra, coluna)
                     mapa_jogador=aloca_navios_jogador(mapa_jogador, CONFIGURACAO[i], coordenada[1], coordenada[0], direcao)
-                    mostrarMapa(mapa_computador, mapa_jogador)
+                    print(printa_string(lista,mapa_jogador))
                     if navios_usuario_2!=[]:
                         del navios_usuario_2[0]
                     break
@@ -205,17 +256,17 @@ def comp_perde(l):
 while True:
     while True:
         linha = random.randint(0, len(mapa_jogador) - 1)
-        coluna = random.randint(0, len(mapa_computador[0]) - 1)
+        coluna = random.randint(0, len(mapa_jogador[0]) - 1)
         if mapa_jogador[linha][coluna]=='   ':
-            mapa_computador[linha][coluna]='A'
+            mapa_jogador[linha][coluna]='A'
             x="Água"
             break
         elif mapa_jogador[linha][coluna]=='\u001b[32m▓▓▓\u001b[0m':
-            mapa_computador[linha][coluna]='X'
+            mapa_jogador[linha][coluna]='X'
             x="Fogo"
             break
     print(f'Computador ----->>>>> {numletra[coluna]}{linha+1} {x}')
-    mostrarMapa(mapa_computador,mapa_jogador)
+    print(printa_string_bombardeio(lista,mapa_jogador,mapa_computador))
     if jogador_perde(mapa_jogador)==True:
         print("Você perdeu!")
         break
@@ -239,7 +290,7 @@ while True:
         else:
             print('Opção inválida')
     print(f'Jogador ----->>>>> {linha}{coluna} {x}')
-    mostrarMapa(mapa_computador,mapa_jogador)
+    print(printa_string_bombardeio(lista,mapa_jogador,mapa_computador))
     if comp_perde(mapa_computador)==True:
         print("Você ganhou! Parabéns!")
         break
